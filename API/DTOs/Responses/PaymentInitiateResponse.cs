4d980011c0d0973896f7066cd0ecac3a61b1f1e7
PratishaBista <bista.pratisha@gmail.com>
2026-01-05 12:13:00 +0545

    feat: payment initiate response DTO

---
+++ API/DTOs/Responses/PaymentInitiateResponse.cs
@@ -0,0 +1,17 @@
+namespace API.DTOs.Responses;
+
+public class PaymentInitiateResponse
+{
+    public string Amount { get; set; } = string.Empty;
+    public string TaxAmount { get; set; } = "0";
+    public string TotalAmount { get; set; } = string.Empty;
+    public string TransactionUuid { get; set; } = string.Empty;
+    public string ProductCode { get; set; } = string.Empty;
+    public string ProductServiceCharge { get; set; } = "0";
+    public string ProductDeliveryCharge { get; set; } = "0";
+    public string SuccessUrl { get; set; } = string.Empty;
+    public string FailureUrl { get; set; } = string.Empty;
+    public string SignedFieldNames { get; set; } = string.Empty;
+    public string Signature { get; set; } = string.Empty;
+    public string EsewaUrl { get; set; } = string.Empty;
+}
